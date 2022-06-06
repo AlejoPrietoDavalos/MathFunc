21d57faaa9b1accc819abd40d2e1bc6b1c72a338
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2022-06-06 00:44:57 -0300

    Grafo Hexagonal Generador
    Se agrega un grafo hexagonal, y una manera automatizada de generar uno con un tamaño arbitrariamente grande. El grafo se crea, genera los vértices automaticamente y determina cuales vértices son adyacentes a éste.

--- Utilidades Matematicas/Grafos/Grafos.py
+++ Utilidades Matematicas/Grafos/Grafos.py
@@ -104,17 +104,121 @@ class Grafo():
 class Vertice():
     def __init__(self, grafo, V_Ady):
 
-        self.grafo = grafo
+        self.grafo = grafo  ########## Creo q no hace falta tener aca guardado el grafo, podemos simplemente usar la función que nos interesa.
         self.ID_Vertice = len(self.grafo.Vertices)   # El primer vertice es el número 0, el segundo será 1, etc..
         self.grafo.AgregarVerticeGrafo(self, V_Ady)
         
 
+class GrafoHexagonal():                 ######### Ver si hereda de grafo.
+    def __init__(self, T_H:int):
+        self.T_H = T_H                                      # Tamaño del hexágono.
+        self.n_filas = 2*T_H-1                              # Número de filas.
         
+        """ Listado con el número de elementos que tiene cada una de las filas del hexágono.
+            Por ejemplo: Un hexágono con T_H=5 devuelve [5,6,7,8,9,8,7,6,5]"""
+        self.n_elem_fila = [self.T_H+i for i in range(self.T_H-1)] + [self.T_H+i for i in range(self.T_H-1, -1, -1)]
+
         
+        """ Estos indices van a ser usados para distinguir en que posición se encuentra cada indice.
+            - Indice Superior: Toda celda con indice <= ind_sup corresponde a la parte superior del hexágono.
+            - Indice Inferior: Toda celda con indice >= ind_inf corresponde a la parte inferior del hexágono.
+            - Indices del Medio: Si ind_sup < indice < ind_inf entonces, la celda corresponde a la parte central."""
+        self.ind_sup = sum(self.n_elem_fila[0:T_H-1]) - 1
+        self.ind_inf = self.ind_sup + 2*T_H
+
+
+        self.grafo = Grafo(sum(self.n_elem_fila))
+        self.GenerarRedHexagonal()
+    
+
+    def GenerarRedHexagonal(self):
+        """Esta función se encarga generar los vértices del grafo viendo quienes son los vértices adyacentes.
+        """
+        for fila in range(self.n_filas):                                # Recorremos cada una de las filas.
+            for i in range(sum(self.n_elem_fila[0:fila]), sum(self.n_elem_fila[0:fila]) + self.n_elem_fila[fila]):      # "i" es el ID del vértice. Generamos los "i" que corresponden a esa fila.
+                V_Ady = self.ComprobarVerticesAdyacentes(fila, i)       # Comprobamos cuales son los vértices adyacentes a este vértice.
+                Vertice(self.grafo, V_Ady)                              # Generamos todos los vértices que el grafo tiene.
+                print(f"i:{i} --> {V_Ady}")
+
+
+    def ComprobarVerticesAdyacentes(self, fila:int, i:int):
+        """Comprueba cuales son los vértices adyacentes a un vértice dado. Devuelve una lista con dichos vértices."""
+        """Método de búsqueda:
+            Observación:
+                Lado Superior:  Si hacemos i-self.n_elem_fila[fila], i-self.n_elem_fila[fila]+1, i+self.n_elem_fila[fila],  y i+self.n_elem_fila[fila]+1.
+                                Asi conseguimos los 4 hexágonos adyacentes. De arriba y abajo. El de la derecha e izquierda es sumar y restar 1.
+                Lado Inferior:  Si hacemos i-self.n_elem_fila[fila], i-self.n_elem_fila[fila]-1, i+self.n_elem_fila[fila],  y i+self.n_elem_fila[fila]-1.
+                                Asi conseguimos los 4 hexágonos adyacentes. De arriba y abajo. El de la derecha e izquierda es sumar y restar 1.
+                Fila Central:   Es un híbrido entre la superior y la inferior. Para ver los de arriba se comporta como Lado Superior, y para los de abajo como el lado Inferior.
         
+        Vamos a revisar, para el vértice dado, la posición de los 6 hexágonos adyacentes.
+        No todos los hexágonos tienen 6 adyacentes, por lo tanto, vamos a hacer las cuentas igual. Y comprobar que ese candidato a celda adyacente
+        este en la fila que realmente debería estar.
+        Ver dibujo explicativo."""
+        V_Ady = []
+        
+        V_Ady += [i-1] if self.FilaPerteneceIndice(i-1)==fila else []                                       # Izquierda
+        V_Ady += [i+1] if self.FilaPerteneceIndice(i+1)==fila else []                                       # Derecha
+
+        if i<=self.ind_sup:     #--------------------LADO SUPERIOR--------------------
+            if not(fila==0):                        # Para la fila 0 no hay que comprobar arriba.
+                ind_UpLeft = i-self.n_elem_fila[fila]
+                V_Ady += [ind_UpLeft] if self.FilaPerteneceIndice(ind_UpLeft)==fila-1 else []               # Arriba Izquierda
+                V_Ady += [ind_UpLeft+1] if self.FilaPerteneceIndice(ind_UpLeft+1)==fila-1 else []           # Arriba Derecha
+            
+            ind_DownLeft = i+self.n_elem_fila[fila]
+            V_Ady += [ind_DownLeft] if self.FilaPerteneceIndice(ind_DownLeft)==fila+1 else []               # Abajo Izquierda
+            V_Ady += [ind_DownLeft+1] if self.FilaPerteneceIndice(ind_DownLeft+1)==fila+1 else []           # Abajo Derecha
+        
+        elif i>=self.ind_inf:   #--------------------LADO INFERIOR--------------------
+            ind_UpRight = i-self.n_elem_fila[fila]
+            V_Ady += [ind_UpRight] if self.FilaPerteneceIndice(ind_UpRight)==fila-1 else []                 # Arriba Izquierda
+            V_Ady += [ind_UpRight-1] if self.FilaPerteneceIndice(ind_UpRight-1)==fila-1 else []             # Arriba Derecha
+            
+            if not(fila==self.n_filas-1):           # Para la ultima fila no hay que comprobar abajo.
+                ind_DownRight = i+self.n_elem_fila[fila]
+                V_Ady += [ind_DownRight] if self.FilaPerteneceIndice(ind_DownRight)==fila+1 else []         # Abajo Izquierda
+                V_Ady += [ind_DownRight-1] if self.FilaPerteneceIndice(ind_DownRight-1)==fila+1 else []     # Abajo Derecha
+        
+        else:                   #--------------------FILA CENTRAL--------------------
+            ind_UpLeft = i-self.n_elem_fila[fila]
+            V_Ady += [ind_UpLeft] if self.FilaPerteneceIndice(ind_UpLeft)==fila-1 else []                   # Arriba Izquierda
+            V_Ady += [ind_UpLeft+1] if self.FilaPerteneceIndice(ind_UpLeft+1)==fila-1 else []               # Arriba Derecha
+
+            ind_DownRight = i+self.n_elem_fila[fila]
+            V_Ady += [ind_DownRight] if self.FilaPerteneceIndice(ind_DownRight)==fila+1 else []             # Abajo Izquierda
+            V_Ady += [ind_DownRight-1] if self.FilaPerteneceIndice(ind_DownRight-1)==fila+1 else []         # Abajo Derecha
+
+        return V_Ady
+    
+    
+    def FilaPerteneceIndice(self, i:int):
+        """ Recibe un indice, y devuelve el número de fila al que pertenece dicho indice.
+        
+        Arg:
+            i (int): Indice del cual queremos saber a que fila pertenece.
+        
+        Return:
+            Devuelve el número de fila en el cual está este indice.
+            Si el indice ingresado es erróneo devuelve -1."""
+        if 0 <= i < self.grafo.CantVertices:      # Éste es el intervalo de valores permitidos.
+            for fila in range(len(self.n_elem_fila)):
+                if i < sum(self.n_elem_fila[0:fila+1]):
+                    return fila
+        else:
+            return -1       # Devuelve -1 si el índice es erróneo.
+        
+
+grafito = GrafoHexagonal(5)
 
 
 
+
+
+
+
+
+'''
 grafo = Grafo(5)
 ListadoVerticesAdyacentes = [[1,2], [0,3], [0,3], [1,2], [1,2]]
 grafo.CrearVertices(ListadoVerticesAdyacentes)
@@ -122,3 +226,4 @@ grafo.CrearVertices(ListadoVerticesAdyacentes)
 
 for v in grafo.Vertices:
     print(v.ID_Vertice)
+'''
